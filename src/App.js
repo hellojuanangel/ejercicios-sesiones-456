00ee81ee35da708343502168316aba96bd46ed2c
JA <jadeveloper21@gmail.com>
2022-09-22 11:47:07 +0200

    init

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Open Bootcamp ReactJS</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ejercicios Sesiones 4, 5 y 6.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
